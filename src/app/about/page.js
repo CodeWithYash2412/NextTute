a2b8e0bbdb9c046c1593302fef2084654afdbc65
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2024-07-10 22:40:14 +0530

    Commit

--- src/app/about/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const aboutinn = () => {
-    return (
-        <div>aboutinn</div>
-    )
-}
-
-export default aboutinn
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 const page = () => {
+  const [shows, setShows] = useState(true)
+
+  const show = () => {
+    setShows(!shows)
+  }
+
   return (
     <div>
-      <h1>this is page 1</h1>
-      <h2> <a href="/about"> second page </a> </h2>
-      <h2> <a href="/blog/522"> dynamic routing </a> </h2>
+      <h1> My Name is Yash </h1>
+      {shows ? <h2> I Am Web Developer </h2> : <h2>Hidden Data</h2>}
+      <button onClick={() => alert("Clicked")}> Click </button>
+      <button onClick={() => show()}> Show / Hide </button>
     </div>
   )
 }
