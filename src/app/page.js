c88d637fd13f72393ec2ce2b5a791d15a104676c
CodeWithYash2412 <84624661+CodeWithYash2412@users.noreply.github.com>
2024-07-11 17:17:15 +0530

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
+'use client'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+  const router = useRouter()
   return (
     <div>
       <h1>this is page 1</h1>
-      <h2> <a href="/about"> second page </a> </h2>
-      <h2> <a href="/blog/522"> dynamic routing </a> </h2>
+      <h2> <Link href="/about"> second page </Link> </h2>
+      <h2> <Link href="/blog/522"> dynamic routing </Link> </h2>
+
+
+      <p>Routing can be done bothway by using Link and useRouter</p>
+
+      <button onClick={() => router.push('/about')}> second page </button>
     </div>
   )
 }
